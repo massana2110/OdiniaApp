b2d71b6fa42b9c0133fb875831fa089874f74b2a
RSanchez19 <00191018@uca.edu.sv>
2020-05-21 21:33:39 -0600

    Vista de ingresos añadida

--- app/src/main/java/com/womannotfound/odinia/views/ui/fragments/operations/EntryMoneyFragment.kt
+++ app/src/main/java/com/womannotfound/odinia/views/ui/fragments/operations/EntryMoneyFragment.kt
@@ -5,20 +5,57 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 import com.womannotfound.odinia.R
 
 /**
  * A simple [Fragment] subclass.
  */
-class EntryMoneyFragment : Fragment() {
+class EntryMoneyFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_entry_money, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_entry_money, container,false)
+        val spinnerAccounts: Spinner = view.findViewById(R.id.spinnerAccounts)
+        val spinnerCategories: Spinner = view.findViewById(R.id.spinnerCategories)
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.accounts,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerAccounts.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            requireContext(),
+            R.array.categories_pays,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerCategories.adapter = adapter
+        }
+
+        spinnerAccounts.onItemSelectedListener = this
+        spinnerCategories.onItemSelectedListener = this
+        return view
     }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val text = parent?.getItemAtPosition(position).toString()
+        Toast.makeText(parent?.context,text,Toast.LENGTH_SHORT).show()
+    }
+
 
 }
