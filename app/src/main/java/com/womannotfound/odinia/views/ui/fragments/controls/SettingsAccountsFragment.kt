d93c6b9c617a49bca32c02477a35003932637065
Rodas20 <00006417@uca.edu.sv>
2020-05-27 11:59:15 -0600

    Adding settings fragments

---
+++ app/src/main/java/com/womannotfound/odinia/views/ui/fragments/controls/SettingsAccountsFragment.kt
@@ -0,0 +1,24 @@
+package com.womannotfound.odinia.views.ui.fragments.controls
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.womannotfound.odinia.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SettingsAccountsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings_accounts, container, false)
+    }
+
+}
