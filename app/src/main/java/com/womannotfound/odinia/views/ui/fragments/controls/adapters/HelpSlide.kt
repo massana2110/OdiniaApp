e2b296dd2fa45613700fa35e436a0c1a78845545
David Massana <00072416@uca.edu.sv>
2020-05-28 16:23:49 -0600

    Add images to HelpFragment

--- app/src/main/java/com/womannotfound/odinia/views/ui/fragments/controls/adapters/HelpSlide.kt
+++ app/src/main/java/com/womannotfound/odinia/views/ui/fragments/controls/adapters/HelpSlide.kt
@@ -1,6 +1,7 @@
 package com.womannotfound.odinia.views.ui.fragments.controls.adapters
 
-data class IntroSlide(
+data class HelpSlide(
     val title: String,
-    val description: String
+    val description: String,
+    val icon: Int
 )
