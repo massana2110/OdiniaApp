ff63a852f6a270220077492a87a70423a80a11c0
AnibalHernandez <00401117@uca.edu.sv>
2020-05-28 19:43:51 -0600

    minor changes to payment form and account form, adding cardView for account view

--- app/src/main/java/com/womannotfound/odinia/views/ui/fragments/operations/ProgrammedPaymentFragment.kt
+++ app/src/main/java/com/womannotfound/odinia/views/ui/fragments/operations/ProgrammedPaymentFragment.kt
@@ -8,6 +8,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -15,14 +16,19 @@ import androidx.navigation.findNavController
 
 import com.womannotfound.odinia.R
 import com.womannotfound.odinia.databinding.FragmentProgrammedPaymentBinding
+import com.womannotfound.odinia.viewmodel.PaymentsViewModel
 
 class ProgrammedPaymentFragment : Fragment() {
-
+    private lateinit var viewModel: PaymentsViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentProgrammedPaymentBinding>(inflater,R.layout.fragment_programmed_payment,container,false)
+
+        viewModel = activity?.run {
+            ViewModelProvider(this, defaultViewModelProviderFactory).get(PaymentsViewModel::class.java)
+        } ?: throw Exception("Invalid Activity")
 
         binding.btnAdd.setOnClickListener{
             val name = binding.namePayment.text.toString()
@@ -34,9 +40,17 @@ class ProgrammedPaymentFragment : Fragment() {
             val year = binding.addDate.year.toString()
 
             val date = "$day/$month/$year"
+            if( (name == " " && amount == " ")|| (name == "" && amount == "")){
+                Toast.makeText(context,"Proporcione datos validos",Toast.LENGTH_SHORT).show()
+            }else{
+                viewModel.name =binding.namePayment.text.toString()
+                viewModel.category =binding.addCategory.selectedItem.toString()
+                viewModel.amount = binding.addAmount.text.toString()
+                viewModel.date =date
 
-            it.findNavController()
-                .navigate(ProgrammedPaymentFragmentDirections.actionProgrammedPaymentFragmentToNavPayments(name,category,amount,date))
+                it.findNavController()
+                    .navigate(ProgrammedPaymentFragmentDirections.actionProgrammedPaymentFragmentToNavPayments())
+            }
         }
 
         ArrayAdapter.createFromResource(
