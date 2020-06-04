4885cc000a0421c44b7795379333188b1fed1447
David Massana <00072416@uca.edu.sv>
2020-06-03 19:31:06 -0600

    Add firebase to the project and layouts of SignIn and SignUp

---
+++ app/src/main/java/com/womannotfound/odinia/views/ui/activities/AuthenticationActivity.kt
@@ -0,0 +1,13 @@
+package com.womannotfound.odinia.views.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.womannotfound.odinia.R
+
+class AuthenticationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
