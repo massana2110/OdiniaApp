a1e324701b60f75ec498f106f3a9258c7a2896e8
David Massana <00072416@uca.edu.sv>
2020-06-06 21:31:16 -0600

    SignIn and SignUp views working

--- app/src/main/java/com/womannotfound/odinia/views/ui/activities/AuthenticationActivity.kt
+++ app/src/main/java/com/womannotfound/odinia/views/ui/activities/AuthenticationActivity.kt
@@ -2,12 +2,21 @@ package com.womannotfound.odinia.views.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.womannotfound.odinia.R
+import com.womannotfound.odinia.views.ui.fragments.authentication.SignInFragment
 
 class AuthenticationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+
+        val fragment = SignInFragment()
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.fragments_container, fragment)
+        fragmentTransaction.commit()
     }
 }
--- app/src/main/java/com/womannotfound/odinia/views/ui/fragments/authentication/SignInFragment.kt
+++ app/src/main/java/com/womannotfound/odinia/views/ui/fragments/authentication/SignInFragment.kt
@@ -1,12 +1,19 @@
 package com.womannotfound.odinia.views.ui.fragments.authentication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 import com.womannotfound.odinia.R
+import com.womannotfound.odinia.databinding.FragmentSignInBinding
+import com.womannotfound.odinia.views.ui.activities.AuthenticationActivity
+import com.womannotfound.odinia.views.ui.activities.MainActivity
 
 /**
  * A simple [Fragment] subclass.
@@ -17,8 +24,25 @@ class SignInFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+        val binding = DataBindingUtil.inflate<FragmentSignInBinding>(inflater,R.layout.fragment_sign_in, container, false)
+
+        binding.signUpNow.setOnClickListener {
+            val signUpFragment: Fragment = SignUpFragment()
+            val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
+            val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragments_container, signUpFragment)
+            fragmentTransaction.addToBackStack(null)
+            fragmentTransaction.commit()
+        }
+
+        binding.signInButton.setOnClickListener {
+            val intent = Intent(requireContext(),
+                MainActivity::class.java)
+
+            startActivity(intent)
+            activity?.finish()
+        }
+        return binding.root
     }
 
 }
