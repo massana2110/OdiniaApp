6477c05f3921939bc4f73772e92e5860cceb384e
Rodas20 <00006417@uca.edu.sv>
2020-07-06 17:03:07 -0600

    Retrieving/Updating payments data from firebase

--- app/src/main/java/com/womannotfound/odinia/viewmodel/PaymentsViewModel.kt
+++ app/src/main/java/com/womannotfound/odinia/viewmodel/PaymentsViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import com.womannotfound.odinia.views.ui.fragments.controls.adapters.PaymentsItems
 
 class PaymentsViewModel : ViewModel(){
-    val list = ArrayList<PaymentsItems>()
     var name: String = ""
     var category: String = ""
     var amount: String = ""
     var date: String = ""
+    val list = ArrayList<PaymentsItems>()
+    var validate : Boolean = false
 }
