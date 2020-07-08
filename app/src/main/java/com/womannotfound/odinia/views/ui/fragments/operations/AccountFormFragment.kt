a4a29f1920a98a938658825951d384a638675924
Rodas20 <00006417@uca.edu.sv>
2020-07-08 03:25:17 -0600

    Adding conditionals to prohibit entering names of existing accounts

--- app/src/main/java/com/womannotfound/odinia/views/ui/fragments/operations/AccountFormFragment.kt
+++ app/src/main/java/com/womannotfound/odinia/views/ui/fragments/operations/AccountFormFragment.kt
@@ -11,6 +11,8 @@ import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 import com.womannotfound.odinia.R
 import com.womannotfound.odinia.databinding.FragmentAccountFormBinding
@@ -19,39 +21,72 @@ import com.womannotfound.odinia.viewmodel.AccountsViewModel
 
 class AccountFormFragment : Fragment() {
     private lateinit var vm: AccountsViewModel
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = DataBindingUtil.inflate<FragmentAccountFormBinding>(inflater,R.layout.fragment_account_form,container,false)
-
+        val binding = DataBindingUtil.inflate<FragmentAccountFormBinding>(
+            inflater,
+            R.layout.fragment_account_form,
+            container,
+            false
+        )
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
         vm = activity?.run {
-            ViewModelProvider(this, defaultViewModelProviderFactory).get(AccountsViewModel::class.java)
+            ViewModelProvider(
+                this,
+                defaultViewModelProviderFactory
+            ).get(AccountsViewModel::class.java)
         } ?: throw Exception("Invalid Activity")
+        val userID = auth.currentUser?.uid.toString()
 
-        binding.btnAdd.setOnClickListener{
+        binding.btnAdd.setOnClickListener {
             vm.name = binding.textEntryNameAcc.text.toString()
-            vm.type= binding.accType.selectedItem.toString()
+            vm.type = binding.accType.selectedItem.toString()
             vm.balance = binding.balanceEntry.text.toString()
 
-            if( (vm.type == "" && vm.balance == "") || vm.type == "" || vm.balance == ""){
-                Toast.makeText(context,"Proporcione datos validos", Toast.LENGTH_SHORT).show()
-            }else{
-                it.findNavController().navigate(AccountFormFragmentDirections.actionNavFormAccountToNavHome())
+
+            var i = 0;
+            db.collection("accounts")
+                .whereEqualTo("userID", userID)
+                .get()
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        if (document.getString("nameAccount").toString() == vm.name) {
+                            i = +1;
+                        }
+                    }
+                    if (i != 0) {
+                        Toast.makeText(
+                            context,
+                            "Error: Nombre de cuenta existente",
+                            Toast.LENGTH_SHORT
+                        ).show()
+
+                    } else if ((vm.type == "" && vm.balance == "") || vm.type == "" || vm.balance == "") {
+                        Toast.makeText(context, "Proporcione datos validos", Toast.LENGTH_SHORT)
+                            .show()
+                    } else {
+                        it.findNavController()
+                            .navigate(AccountFormFragmentDirections.actionNavFormAccountToNavHome())
+                    }
+
+                }
+        }
+
+            ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.types_of_accounts,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                binding.accType.adapter = adapter
             }
 
+            return binding.root
         }
 
-        ArrayAdapter.createFromResource(
-            requireContext(),
-            R.array.types_of_accounts,
-            android.R.layout.simple_spinner_item
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            binding.accType.adapter = adapter
-        }
-        
-        return binding.root
     }
-
-}
