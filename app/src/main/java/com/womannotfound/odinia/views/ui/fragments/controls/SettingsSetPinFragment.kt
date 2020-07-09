93c6b90be49688e852de490dfd1bbffb234df8ce
Rodas20 <00006417@uca.edu.sv>
2020-07-09 03:05:03 -0600

    Adding functionality to setPin settings

--- app/src/main/java/com/womannotfound/odinia/views/ui/fragments/controls/SettingsSetPinFragment.kt
+++ app/src/main/java/com/womannotfound/odinia/views/ui/fragments/controls/SettingsSetPinFragment.kt
@@ -1,35 +1,70 @@
 package com.womannotfound.odinia.views.ui.fragments.controls
 
+import android.content.ContentValues
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 import com.womannotfound.odinia.R
 import com.womannotfound.odinia.databinding.FragmentSettingsSetPinBinding
+import com.womannotfound.odinia.viewmodel.AccountsViewModel
+import com.womannotfound.odinia.viewmodel.PaymentsViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class SettingsSetPinFragment : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding =DataBindingUtil.inflate<FragmentSettingsSetPinBinding>(inflater,
-        R.layout.fragment_settings_set_pin, container, false)
+        val binding = DataBindingUtil.inflate<FragmentSettingsSetPinBinding>(
+            inflater,
+            R.layout.fragment_settings_set_pin, container, false
+        )
 
-        binding.btnAdd.setOnClickListener{
+        auth = FirebaseAuth.getInstance()
+        db = FirebaseFirestore.getInstance()
+        val userID = auth.currentUser?.uid.toString()
 
-            val password = binding.editText.text.toString()
 
-            it.findNavController().navigate(SettingsSetPinFragmentDirections.actionNavSettingsSetPinFragmentToNavSettings(password))
+        binding.btnAdd.setOnClickListener {
+            var passwordPin = binding.editText.text.toString()
+
+
+            if (passwordPin == "") {
+                Toast.makeText(context, "Proporcione datos validos", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                db.collection("users")
+                    .document(userID)
+                    .update("userPin", passwordPin)
+                it.findNavController()
+                    .navigate(SettingsSetPinFragmentDirections.actionNavSettingsSetPinFragmentToNavSettings())
+                Toast.makeText(
+                    context,
+                    "Pin editado exitosamente",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+                passwordPin=""
+            }
 
         }
+
 
         return binding.root
     }
